ba220ae1dd6fa8475e0f0ec8b41f280b2cb82949
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-01-06 23:40:11 +0200

    script goes through the letters

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const article = document.getElementsByTagName('body')[0]
+  .appendChild(document.createElement('article'));
+const text = 'I am Super Man!';
+article.innerHTML = text.split('')
+  .map((char) => `<div class='item'>${char}</div>`)
+  .join('\n');
+article.classList.add('word-wrapper');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const nodes = document
+  .querySelector('.word-wrapper')
+  .children;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+for (let i = 0, n = 0; i < text.length; i++) {
+  if (text.charAt(i) !== ' ') {
+    const invokeClassToggle = () => {
+        nodes[i].classList.add('active');
+        setTimeout(
+          () => nodes[i].classList.remove('active'),
+          1000
+        );
+
+        setTimeout(
+          invokeClassToggle,
+          text.replace(/ /g, '').length * 1000
+        );
+      };
+
+    setTimeout(
+      invokeClassToggle,
+      1000 + (n * 1000)
+    );
+
+    if (n < text.replace(/ /g, '').length) {
+      n++;
+    }
+  }
+}
