25dc5b028aa0ce7b0b7b57ce0ca1cd8aa282870b
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-01-09 15:36:49 +0200

    CssGoesOverChars Module

---
+++ src/components/CssGoesOverChars/CssGoesOverChars.js
@@ -0,0 +1,40 @@
+export default class CssGoesOverChars {
+  start(elementId) {
+    const article = document.getElementById(elementId);
+    const text = article.innerHTML;
+    article.innerHTML = text.split('')
+      .map((char) => `<div class='item'>${char}</div>`)
+      .join('\n');
+    article.classList.add('word-wrapper');
+
+    const nodes = document
+      .querySelector('.word-wrapper')
+      .children;
+
+    for (let i = 0, n = 0; i < text.length; i++) {
+      if (text.charAt(i) !== ' ') {
+        const invokeClassToggle = () => {
+          nodes[i].classList.add('active');
+          setTimeout(
+            () => nodes[i].classList.remove('active'),
+            1000
+          );
+
+          setTimeout(
+            invokeClassToggle,
+            text.replace(/ /g, '').length * 1000
+          );
+        };
+
+        setTimeout(
+          invokeClassToggle,
+          1000 + (n * 1000)
+        );
+
+        if (n < text.replace(/ /g, '').length) {
+          n++;
+        }
+      }
+    }
+  }
+};
