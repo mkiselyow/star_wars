7c20523e33a2499cdf39f6ef4c8d2a430f6c2dcc
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-01-11 20:14:36 +0200

    hide progress indicator onFetch ended

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,16 @@ import SwapiService from '../SwapiService/SwapiService';
 import CssGoesOverChars from '../CssGoesOverChars/CssGoesOverChars';
 
 export default class App extends Component {
+  isFetchInProgress = false;
+
+  changeProgress = (boolean) => {
+    this.isFetchInProgress = boolean;
+    if (document.getElementById('progress')) {
+      document.getElementById('progress')
+        .style.visibility = this.isFetchInProgress ? 'visible' : 'hidden';
+    }
+  };
+
   render() {
     const article = document.getElementsByTagName('body')[0]
       .appendChild(document.createElement('article'));
@@ -10,10 +20,21 @@ export default class App extends Component {
     article.innerHTML = 'I am Super Man!';
     CssGoesOverChars.prototype.start(article.id);
 
+    this.changeProgress(true);
     (new SwapiService()).getResource(undefined, undefined,1)
-      .then((body) => console.log(body))
-      .catch((err) => console.log(err.message));
+      .then((body) => {
+        this.changeProgress(false);
+        console.log(body);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        this.changeProgress(false);
+      });
 
-    return <progress value="50" max="1000"></progress>
+    return (
+      <div id='progress' className='d-flex justify-content-center align-items-center'>
+        <progress max="10">i</progress>
+      </div>
+    )
   }
 };
