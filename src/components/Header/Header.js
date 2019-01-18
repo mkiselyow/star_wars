184a4081fecbb752e90a1b325910fdc3323ac552
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-01-18 15:49:55 +0200

    Header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,18 @@ import './Header.css';
 export default class Header extends Component {
   render() {
     return (
-      <header className='header d-flex justify-content-center'>
-        People Planet Starships
+      <header className='header'>
+        <ul className="nav nav-list-item-container">
+          <li className="nav-item">
+            <a className="nav-link nav-list-item" href="#">People</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link nav-list-item" href="#">Planets</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link nav-list-item" href="#">Ships</a>
+          </li>
+        </ul>
       </header>
     )
   }
