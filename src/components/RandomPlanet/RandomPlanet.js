7cd41e61f531260786397dc7539930a32e915cfc
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-02-06 00:04:20 +0200

    random planet component state

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,31 +1,67 @@
 import React, {Component} from 'react';
+
+import SwapiService from '../../services/SwapiService/SwapiService'
+
 import './RandomPlanet.css';
 
 export default class RandomPlanet extends Component {
+
+  swapiService = new SwapiService();
+
+  state = {
+    id: null,
+    name: null,
+    population: null,
+    rotationPeriod: null,
+    diameter: null
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random()*25) + 2;
+    this.swapiService
+      .getResource('planets', id)
+      .then((planet) => {
+        this.setState({
+          id: id,
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotation_period,
+          diameter: planet.diameter
+        })
+      })
+  };
+
   render() {
+    const {id, name, population, rotationPeriod, diameter} = this.state;
+
     return (
       <article className='person-details container bg-gray rounded'>
         <div className='row'>
           <div className='col-12 col-sm-6 d-flex justify-content-center align-items-center'>
-            <img className='rounded my-2' src="https://vignette.wikia.nocookie.net/starwars/images/a/af/Mustafar_DB.png/revision/latest?cb=20160118061913" alt=""/>
+            <img className='rounded my-2 random-planet-img' src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt=""/>
           </div>
           <div className='col-12 col-sm-6 d-flex flex-column justify-content-center align-content-middle'>
             <header className='mt-auto mb-auto'>
-              <span>Random Planet Name</span>
+              <span>{name}</span>
             </header>
             <table className="table table-hover">
               <tbody>
                 <tr className="table-active">
                   <th scope="row">Population</th>
-                  <td>1000000</td>
+                  <td>{population}</td>
                 </tr>
                 <tr className="table-active">
                   <th scope="row">Rotation Period</th>
-                  <td>22</td>
+                  <td>{rotationPeriod}</td>
                 </tr>
                 <tr className="table-active">
                   <th scope="row">Diameter</th>
-                  <td>1000</td>
+                  <td>{diameter}</td>
                 </tr>
               </tbody>
             </table>
