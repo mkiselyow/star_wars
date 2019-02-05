6e6a7c1ddef039cdee0e3cb8a16fbf4cf1c79ae1
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-02-05 22:14:28 +0200

    content styling

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -9,10 +9,26 @@ export default class RandomPlanet extends Component {
           <div className='col-12 col-sm-6 d-flex justify-content-center align-items-center'>
             <img className='rounded my-2' src="https://vignette.wikia.nocookie.net/starwars/images/a/af/Mustafar_DB.png/revision/latest?cb=20160118061913" alt=""/>
           </div>
-          <div className='col-12 col-sm-6 d-flex justify-content-center align-content-middle'>
+          <div className='col-12 col-sm-6 d-flex flex-column justify-content-center align-content-middle'>
             <header className='mt-auto mb-auto'>
               <span>Random Planet Name</span>
             </header>
+            <table className="table table-hover">
+              <tbody>
+                <tr className="table-active">
+                  <th scope="row">Population</th>
+                  <td>1000000</td>
+                </tr>
+                <tr className="table-active">
+                  <th scope="row">Rotation Period</th>
+                  <td>22</td>
+                </tr>
+                <tr className="table-active">
+                  <th scope="row">Diameter</th>
+                  <td>1000</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </article>
