70e666ba9fa374b5ebfea1b6ee26e1e5a3a3a03e
mkyse <mkyse@softserveinc.com>
2019-02-28 13:58:38 +0200

    Updates

---
+++ precache-manifest.dc74465faa6f8a4dca02079141632fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "87520128dbc2485c5b69a15d5a65940d",
+    "url": "/star_wars/static/media/satan_hum_sav.87520128.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/star_wars/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "b5d694a2815a56bdcbb0",
+    "url": "/star_wars/static/js/runtime~main.b5d694a2.js"
+  },
+  {
+    "revision": "d6e5016c509619465027",
+    "url": "/star_wars/static/js/main.d6e5016c.chunk.js"
+  },
+  {
+    "revision": "770e40c501493aff766b",
+    "url": "/star_wars/static/js/1.770e40c5.chunk.js"
+  },
+  {
+    "revision": "d6e5016c509619465027",
+    "url": "/star_wars/static/css/main.8ef95e47.chunk.css"
+  },
+  {
+    "revision": "db68f17d175b161d10b7324f21e9dbb8",
+    "url": "/star_wars/index.html"
+  }
+];
