1b23113b3c2cd50fa5253bb2e5bf18126d69dbbf
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-03-20 23:56:44 +0200

    ItemList population

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,21 +1,41 @@
 import React, {Component} from 'react';
 import './ItemList.css';
+import SwapiService from "../../services/SwapiService/SwapiService";
 
 export default class ItemList extends Component {
+
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: null
+  };
+
+  componentDidMount() {
+    this.swapiService
+      .getResource('people')
+      .then((people) => {
+        this.setState({peopleList: people});
+      })
+      .catch(this.onError);
+  }
+
+  prepareList() {
+    if (this.state.peopleList) {
+      return this.state.peopleList.map((person) => {
+        return (
+          <ItemListLink href={"#"} title={person.name}/>
+        )
+      })
+    }
+  }
+
   render() {
+    const peopleList = this.prepareList();
+
     return (
       <article className='itemList my-2'>
         <ul className='list-group'>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
-          <ItemListLink href={"#"} title="Item"/>
+          {peopleList}
         </ul>
       </article>
     )
