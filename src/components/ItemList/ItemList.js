fdccd05eb8157fdea1c0d71642027ac2b03456dc
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-05-01 10:06:18 +0300

    ItemList to function

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,44 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './ItemList.css';
-import SwapiService from "../../services/SwapiService/SwapiService";
 import Spinner from "../Spinner/Spinner"
 import Error from "../Error/Error";
+import withData from "../hoc-helpers/withData";
 
-class ItemList extends Component {
+const ItemList = ({onItemSelected, data, error}) => {
 
-  prepareList() {
-    if (this.props.data) {
-      return this.props.data.map((item) => {
+  const prepareList = () => {
+    if (data) {
+      return data.map((item) => {
         return (
           <ItemListLink
             key={item.id}
             href={"#"}
             title={renderItem.call(this, item)}
             id={item.id}
-            onClick={() => this.props.onItemSelected(item.id)}
+            onClick={() => onItemSelected(item.id)}
           />
         )
       })
     }
-  }
+  };
 
-  render() {
-    const { data, error } = this.props;
-    const content = data ? this.prepareList() : null;
-    const spinner = (!data && !error) ? <Spinner/> : null;
-    const errorMessage = error ? <Error/> : null;
+  const content = data ? prepareList() : null;
+  const spinner = (!data && !error) ? <Spinner/> : null;
+  const errorMessage = error ? <Error/> : null;
 
-    return (
-      <article className='itemList my-2'>
-        <ul className='list-group'>
-          {spinner}
-          {content}
-          {errorMessage}
-        </ul>
-      </article>
-    )
-  }
-}
+  return (
+    <article className='itemList my-2'>
+      <ul className='list-group'>
+        {spinner}
+        {content}
+        {errorMessage}
+      </ul>
+    </article>
+  )
+};
 
 const ItemListLink = ({title, onClick}) => {
   return (
@@ -53,7 +50,7 @@ const ItemListLink = ({title, onClick}) => {
 };
 
 const renderItem = function(item) {
-  switch (this.props.itemsType) {
+  switch (item.itemsType) {
     case 'people':
       return `${item.name} (gender: ${item.gender})`;
     case 'starships':
@@ -62,37 +59,6 @@ const renderItem = function(item) {
       return `${item.name} (diameter: ${item.diameter})`;
     default:
       return item.name;
-  }
-};
-
-const withData = (View) => {
-  return class extends Component {
-
-    swapiService = new SwapiService();
-
-    state = {
-      data: null
-    };
-
-    componentDidMount() {
-      this.swapiService
-        .getAllResources(this.props.itemsType)
-        .then((items) => {
-          this.setState({data: items});
-        })
-        .catch(this.onError);
-    };
-
-    onError = () => {
-      this.setState({error: true});
-    };
-
-    render() {
-      const props = this.props;
-      const { data } = this.state;
-
-      return <View {...props} data={data}/>;
-    }
   }
 };
 
