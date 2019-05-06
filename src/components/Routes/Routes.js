3ff53d899c64ea9f1c3c3968e1cb910d33520778
Maxim Kiselyow <m.kiselyow@gmail.com>
2019-05-06 08:29:31 +0300

    Switch component

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,56 +3,62 @@ import ItemsPage from "../ItemsPage/ItemsPage";
 import ItemDetails from "../ItemDetails/ItemDetails";
 import SecretPage from  "../SecretPage/SecretPage";
 import LoginPage from  "../LoginPage/LoginPage";
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 const Routes = ({isLoggedIn, onLogin}) => {
   const preparedRoutes = ['planets', 'starships', 'people']
     .map((route) => {
     return (
       <React.Fragment key={route}>
-        <Route path={`/${route}/selectedItem/:id?`}
-               component={() =>
-                 <ItemsPage itemsType={route}/>}
-               exact
-        />
-        <Route path={`/${route}/`}
-               component={() =>
-                 <ItemsPage itemsType={route}/>}
-               exact
-        />
-        <Route path={`/${route}/:id([0-9]+)`}
-          render={() => {
-            return <ItemDetails itemType={route}
-                                isFullDescription={true}
-            />}}
-        />
+        <Switch>
+          <Route path={`/${route}/selectedItem/:id?`}
+                 component={() =>
+                   <ItemsPage itemsType={route}/>}
+                 exact
+          />
+          <Route path={`/${route}/`}
+                 component={() =>
+                   <ItemsPage itemsType={route}/>}
+                 exact
+          />
+          <Route path={`/${route}/:id([0-9]+)`}
+            render={() => {
+              return <ItemDetails itemType={route}
+                                  isFullDescription={true}
+              />}}
+          />
+          <Redirect to="/"/>
+        </Switch>
       </React.Fragment>
     )
   });
 
   return (
     <React.Fragment>
-      <Route path="/"
-             render={
-               () => { return (
-                 <h2 className="text-center my-3">
-                   Welcome to initial page
-                 </h2>)}}
-             exact/>
-      <Route path={`/secret/`}
-             component={() =>
-               <SecretPage isLoggedIn={isLoggedIn}/>}
-             exact
-      />
-      <Route path={`/login/`}
-             component={() =>
-               <LoginPage isLoggedIn={isLoggedIn}
-                          onLogin={() => {onLogin()}}
-               />}
-             exact
-      />
+      <Switch>
+        <Route path="/"
+               render={
+                 () => { return (
+                   <h2 className="text-center my-3">
+                     Welcome to initial page
+                   </h2>)}}
+               exact/>
+        <Route path={`/secret/`}
+               component={() =>
+                 <SecretPage isLoggedIn={isLoggedIn}/>}
+               exact
+        />
+        <Route path={`/login/`}
+               component={() =>
+                 <LoginPage isLoggedIn={isLoggedIn}
+                            onLogin={() => {onLogin()}}
+                 />}
+               exact
+        />
 
-      {preparedRoutes}
+        {preparedRoutes}
+        <Redirect to="/"/>
+      </Switch>
     </React.Fragment>
   )
 };
